8b741e57514ae1d4353ce83376b1ba56f0831a87
Ismail Ibrahim Quwarah <iquwarah@redhat.com>
2024-06-19 11:44:01 +0200

    Add `epel` coverage test in tmt try

--- tests/try/basic/test.sh
+++ tests/try/basic/test.sh
@@ -27,6 +27,12 @@ rlJournalStart
         rlAssertGrep "Run .* successfully finished. Bye for now!" $rlRun_LOG
     rlPhaseEnd
 
+    rlPhaseStartTest "Epel"
+        rlRun -s "./epel.exp"
+        rlAssertGrep "Let's try.*/plans/basic" $rlRun_LOG
+        rlAssertGrep "Run .* successfully finished. Bye for now!" $rlRun_LOG
+    rlPhaseEnd
+
     rlPhaseStartTest "Verbose Output"
         rlRun -s "TMT_CONFIG_DIR=$config ./verbose.exp"
         rlAssertGrep "custom-prepare" $rlRun_LOG
